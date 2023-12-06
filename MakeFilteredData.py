2c1a01a91d8e94afdd3f4492ba6e832c4d02fe08
2018147579 <qjaqnd1357@yonsei.ac.kr>
2023-12-07 04:06:11 +0900

    Project

---
+++ MakeFilteredData.py
@@ -0,0 +1,81 @@
+import pandas as pd         # .csv 파일을 가공하기 위함
+import geopandas as gpd     # .shp 파일을 가공하기 위함
+import os                   # Filtered_Data 디렉토리를 생성하기 위함
+
+# 0. 데이터를 내보내기 전에 'Filtered_Data' 폴더가 있는지 확인하고, 없다면 생성
+output_directory = './Filtered_Data/'
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+# 1. 서울특별시 공공자전거 대여소별 정보를 서대문구 한정의 정보로 가공하기
+# 원본 데이터셋 불러오기
+df = pd.read_csv('./Original_Data/서울특별시 공공자전거 대여소별 이용정보(월별)_23.1-6.csv', index_col=False, encoding='cp949')
+
+# 지역을 서대문구로 한정해서 데이터 추출하곡 이름 형식 정렬하기
+df1 = df.loc[(df.자치구 == '서대문구'), :]
+df1_copy = df1.copy()
+extracted_st_name = df1_copy['대여소명'].str.extract(r'(\d+)\.\s*(.*)')
+df1_copy['대여소명'] = extracted_st_name[0] + ". " + extracted_st_name[1]
+
+# '대여건수'와 '반납건수' 열의 데이터를 정수형으로 변환
+df1_copy['대여건수'] = df1_copy['대여건수'].str.replace(',', '').astype(int)
+df1_copy['반납건수'] = df1_copy['반납건수'].str.replace(',', '').astype(int)
+
+# '기준년월'이 202304인 행 필터링
+filtered_df1 = df1_copy[df1_copy['기준년월'] == 202304].copy()
+
+# 대여건수와 반납건수의 차이 & 대여소별 유동인구 계산
+filtered_df1['대여반납차이'] = filtered_df1['대여건수'] - filtered_df1['반납건수']
+filtered_df1['이용건수'] = filtered_df1['대여건수'] + filtered_df1['반납건수']
+
+# 가공된 데이터 내보내기
+filtered_df1.to_csv('./Filtered_Data/서대문구 대여소별 이용정보(2304).csv', encoding="utf-8-sig", index=False)
+
+# 서대문구 내 대여소 이름 가져오기
+unique_stations = filtered_df1['대여소명'].unique()
+
+# 2. 서울시 따릉이대여소 마스터 정보를 서대문구 한정의 정보로 가공하기
+# 원본 데이터셋 불러오기
+df_loc = pd.read_csv('./Original_Data/공공자전거 대여소 정보(23.06월 기준).csv', index_col=False, encoding='cp949')
+
+# 필요없는 행과 열 잘라내기
+filtered_df_loc = df_loc.iloc[4: , :6]
+
+# 1번에서는 대여소명에 대여소 번호와 이름이 합쳐져있는데 2번의 원본 데이터는 번호와 이름이 다른 컬럼에 들어가있다.
+# 따라서 두 데이터셋의 포맷을 같게 해주기 위해 컬럼을 합쳐 기존의 두 컬럼과 바꿔준다.
+filtered_df_loc['대여소\n번호'] = filtered_df_loc['대여소\n번호'].astype(str)
+filtered_df_loc['보관소(대여소)명'] = filtered_df_loc['보관소(대여소)명'].astype(str)
+filtered_df_loc['대여소명'] = filtered_df_loc['대여소\n번호'].str.split('.').str[0] + '. ' + filtered_df_loc['보관소(대여소)명']
+
+# 정규 표현식을 사용하여 숫자와 이름 분리
+extracted_st_name2 = filtered_df_loc['대여소명'].str.extract(r'(\d+)\.\s*(.*)')
+
+# 숫자와 이름을 결합하여 새로운 '대여소명' 형식 생성
+filtered_df_loc['대여소명'] = extracted_st_name2[0] + ". " + extracted_st_name2[1]
+
+# 필요없는 컬럼 삭제 후 대여소명을 제일 첫 컬럼으로 옮겨주기
+filtered_df_loc = filtered_df_loc.iloc[: , 2:]
+filtered_df_loc = filtered_df_loc.drop(columns=['Unnamed: 3'])
+column_order = ['대여소명'] + [col for col in filtered_df_loc.columns if col != '대여소명']
+filtered_df_loc = filtered_df_loc[column_order]
+
+# 다른 컬럼명 올바르게 수정하기
+filtered_df_loc.rename(columns={'소재지(위치)': '자치구'}, inplace=True)
+filtered_df_loc.rename(columns={'Unnamed: 4': '위도'}, inplace=True)
+filtered_df_loc.rename(columns={'Unnamed: 5': '경도'}, inplace=True)
+
+# 자치구를 서대문구로 한정하기
+filtered_df_loc = filtered_df_loc.loc[(filtered_df_loc.자치구 == '서대문구'), :]
+
+# 가공된 데이터 내보내기
+filtered_df_loc.to_csv('./Filtered_Data/서대문구 따릉이대여소 위치정보.csv', encoding="utf-8-sig", index=False)
+
+# 3. 전국 시군구 단위의 지리적 정보를 서대문구 한정의 정보로 가공하기
+# 원본 데이터셋 가져오기
+sig = gpd.read_file('./Original_Data/sig.shp', encoding='ANSI')
+
+# 서대문구 지역만 필터링
+seodaemun_gu = sig[sig['SIG_KOR_NM'] == '서대문구']
+
+# 서대문구 데이터를 새로운 SHP 파일로 저장
+seodaemun_gu.to_file('./Filtered_Data/seodaemun_gu.shp', encoding='UTF-8')
---
+++ main.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from pyproj import Transformer
+from geopandas.tools import sjoin
+
+# 위도 경도 좌표를 UTM-K(GRS80 타원체) 좌표로 변환하는 함수
+def convert_to_utm_k(lat, lon):
+    transformer = Transformer.from_crs("epsg:4326", "epsg:5179", always_xy=True)
+    x, y = transformer.transform(lon, lat)
+    return x, y
+
+# 가공된 데이터 가져오기
+df1 = pd.read_csv('./Filtered_Data/서대문구 대여소별 이용정보(2304).csv', index_col=False)
+df2 = pd.read_csv('./Filtered_Data/서대문구 따릉이대여소 위치정보.csv', index_col=False)
+seodaemungu_geo = gpd.read_file('./Filtered_Data/seodaemun_gu.shp', encoding='UTF-8')
+
+# 대여반납차이가 큰 상위 10개의 대여소 추출
+top_10_diff = df1.sort_values(by='대여반납차이', ascending=False).head(10)
+
+# top_10_diff의 대여소명을 기반으로 df2에서 해당 대여소의 위도와 경도를 추출
+top_10_loc = df2[df2['대여소명'].isin(top_10_diff['대여소명'])]
+top_10_loc_copy = top_10_loc.copy()
+
+# 상위 10개를 제외한 나머지 대여소들의 위치정보 추출
+remain_loc = df2[~df2['대여소명'].isin(top_10_diff['대여소명'])]
+remain_loc_copy = remain_loc.copy()
+
+# 위도와 경도를 WGS 84에서 UTM-K로 변환
+for idx, row in top_10_loc_copy.iterrows():
+    x, y = convert_to_utm_k(row['위도'], row['경도'])
+    top_10_loc_copy.loc[idx, 'UTM-K_X'] = x
+    top_10_loc_copy.loc[idx, 'UTM-K_Y'] = y
+
+for idx, row in remain_loc_copy.iterrows():
+    x, y = convert_to_utm_k(row['위도'], row['경도'])
+    remain_loc_copy.loc[idx, 'UTM-K_X'] = x
+    remain_loc_copy.loc[idx, 'UTM-K_Y'] = y
+
+# 위도와 경도를 기반으로 GeoDataFrame 생성
+gdf = gpd.GeoDataFrame(
+    top_10_loc, 
+    geometry=gpd.points_from_xy(top_10_loc_copy['UTM-K_X'], top_10_loc_copy['UTM-K_Y'])
+)
+
+# 나머지 대여소의 GeoDataFrame 생성
+remain_gdf = gpd.GeoDataFrame(
+    remain_loc, 
+    geometry=gpd.points_from_xy(remain_loc_copy['UTM-K_X'], remain_loc_copy['UTM-K_Y'])
+)
+
+# 모든 대여소의 위치 정보를 포함하는 GeoDataFrame 생성
+all_loc_copy = df2.copy()
+for idx, row in all_loc_copy.iterrows():
+    x, y = convert_to_utm_k(row['위도'], row['경도'])
+    all_loc_copy.loc[idx, 'UTM-K_X'] = x
+    all_loc_copy.loc[idx, 'UTM-K_Y'] = y
+
+all_gdf = gpd.GeoDataFrame(
+    all_loc_copy, 
+    geometry=gpd.points_from_xy(all_loc_copy['UTM-K_X'], all_loc_copy['UTM-K_Y'])
+)
+
+# 상위 10개 대여소 주변의 버퍼 생성
+buffer_radius = 300  # 300미터, 한 대여소 기준 300미터 아느이 대여소 개수를 판별하고자 함
+top_10_gdf_buffer = gdf.copy()
+top_10_gdf_buffer['geometry'] = top_10_gdf_buffer.geometry.buffer(buffer_radius)
+
+# 공간 조인을 사용하여 버퍼 내의 나머지 대여소 식별
+within_buffer = sjoin(all_gdf, top_10_gdf_buffer, how="inner", predicate='intersects')
+
+# 버퍼 내의 대여소 수 계산
+buffer_counts = within_buffer.groupby('index_right').size()
+
+# 버퍼 내의 대여소가 특정 임계값보다 적은 지점 식별, 자신 포함 3개 미만이면 대여소가 부족한 것으로 판단
+low_density_areas = buffer_counts[buffer_counts < 3].index.tolist()
+
+# 낮은 밀도 지역의 대여소 식별
+low_density_station = top_10_gdf_buffer.loc[low_density_areas]
+
+# 시각화
+fig, ax = plt.subplots()
+seodaemungu_geo.plot(ax=ax, color='white', edgecolor='black')
+
+# 상위 10개 대여소에 버퍼 영역 표시 (녹색으로 표시)
+top_10_gdf_buffer.plot(ax=ax, color='green', alpha=0.1, edgecolor='green')
+
+# low_density_station에 해당하는 대여소의 버퍼를 붉은색으로 표시 (이미 버퍼가 적용된 지오메트리를 사용)
+low_density_station.plot(ax=ax, color='red', alpha=0.5, edgecolor='red')
+
+# 각 대여소 위치에 대여소의 인덱스 레이블을 텍스트로 마킹
+for idx, row in gdf.iterrows():
+    ax.text(row.geometry.x, row.geometry.y, str(idx), fontsize=12, ha='right')
+
+# 각 대여소 마커 표시
+gdf.plot(ax=ax, color='red', markersize=50)  # 상위 10개 대여소
+remain_gdf.plot(ax=ax, color='blue', markersize=20)  # 나머지 대여소
+
+plt.show()
